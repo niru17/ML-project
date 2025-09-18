a0a0f7867b391db9f3b62f24776efa61f0024c91
Niranjana Subramanian <subramanianniranjana@gmail.com>
2025-09-17 19:56:17 -0500

    Model trainer and utils function

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 import pickle
 
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -16,3 +18,33 @@ def save_object(file_path, obj):
         
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
+    try:
+        report={}
+        for model_name, model in models.items():
+
+            # get parameter grid for this model
+            para = param.get(model_name, {})
+
+            if para:  # if hyperparameters provided
+                gs = GridSearchCV(model, para, cv=3, n_jobs=-1, scoring="r2")
+                gs.fit(X_train, y_train)
+                best_model = gs.best_estimator_
+            else:
+                model.fit(X_train, y_train)
+                best_model = model
+
+            # evaluate
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score  
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e,sys)
